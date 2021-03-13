d3fb609f86efaa1e9b8de8f65bc230da8fb84c59
Sourav R <developer.srv1@gmail.com>
2021-03-13 19:10:11 +0530

    inplace package use test

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,36 @@
+@extends('app')
+
+@section('content')
+
+    <div class="max-w-6xl mx-auto sm:px-6 lg:px-8">
+        <x-inplace-component
+            model="users:1"
+            :inline="true"
+            validation="required|min:10"
+        >
+        Lorem Ipsum
+        </x-inplace-component>
+    </div>
+
+    <div class="max-w-6xl mx-auto sm:px-6 lg:px-8">
+        <x-inplace-component
+            model="users:1"
+            :inline="true"
+            validation="required|min:10"
+        >
+        <x-slot name="before"><div class="myclass anotherclass"><h2></x-slot>
+        <x-slot name="after"></h2></div></x-slot>
+
+        Lorem Ipsum is simply Souravd
+        </x-inplace-component>
+    </div>
+
+    <div class="max-w-6xl mx-auto sm:px-6 lg:px-8">
+        <x-inplace-component
+            model="users:1"
+            :inline="true"
+            value="hello there"
+        />
+    </div>
+
+@endsection
