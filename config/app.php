2da3f116144e123a57c4dcdf9764d24a89ad26a6
Sourav R <developer.srv1@gmail.com>
2021-04-17 12:26:25 +0530

    register inplace provider

--- config/app.php
+++ config/app.php
@@ -165,6 +165,7 @@ return [
         /*
          * Package Service Providers...
          */
+        App\Providers\InplaceConfigServiceProvider::class,
 
         /*
          * Application Service Providers...
